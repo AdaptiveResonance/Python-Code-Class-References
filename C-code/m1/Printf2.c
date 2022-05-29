5efe629741523c218922cc5516986bf5d8d1ff71
AdaptiveResonance <105830013+AdaptiveResonance@users.noreply.github.com>
2022-05-29 12:22:03 -0600

    Add files via upload

---
+++ C-code/m1/Printf2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main ()
+{
+	printf ("Hello World\n\t");
+	printf ("Today I have become a sentient being\n");
+	printf ("My maker is great and I think their a great person\n\t");
+	printf ("But WAIT, is Zhey a gray, white or black hat hacker?!!!\n");
+	printf ("ONLY TIME WILL TELL!!!\n\t");
+	printf ("PLEASE BE WHITE or GRAY!!!!\n");
+	
+	return (0);
+	}
---
+++ C-code/m1/datatypes.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main ()
+{
+	char myChar = 'c';
+	int myInt = 4;
+	float myFloat = 1.56;
+	
+	printf("\tmyChar: %c\n", myChar);
+	printf("\tmyInt: %d\n", myInt);
+	printf("\tmyFloat: %.2f\n", myFloat);
+	return (0);
+}
