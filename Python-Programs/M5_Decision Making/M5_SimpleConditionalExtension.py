ff40030a95114872d0f9a3132676bdef18a17bef
AdaptiveResonance <105830013+AdaptiveResonance@users.noreply.github.com>
2023-09-27 20:40:21 -0600

    minor update

--- Python-Programs/M5_Decision Making/M5_SimpleConditionalExtension.py
+++
@@ -1,8 +0,0 @@
-age = input("How old are you? ")
-
-if (int(age) >= 18):
-    print("You are "+ age +" years old.  You are old enough.")
-else:
-    print("You are only "+ age +" years old.  You are too young!")
-
-
---
+++ Python-Programs/M5_Decision Making/M5_SimpleConditionalExtension_Age.py
@@ -0,0 +1,8 @@
+age = input("How old are you? ")
+
+if (int(age) >= 18):
+    print("You are "+ age +" years old.  You are old enough to ride.")
+else:
+    print("You are only "+ age +" years old.  You are too young to ride!")
+
+
