4007bc853fe51bb7fa29b5088da6fb2b1ce5d316
AdaptiveResonance <105830013+AdaptiveResonance@users.noreply.github.com>
2026-03-25 12:19:39 -0600

    updated M9 slightly with better notation

--- Python-Programs/M9_File_Manipulation/File_Exists_WhereM9.py
+++ Python-Programs/M9_File_Manipulation/File_Exists_WhereM9.py
@@ -10,9 +10,9 @@ file_path = os.path.join(script_dir, "Eggs.txt")
 
 
 if os.path.exists("Eggs.txt"):      #check the current terminal working directory for the text file
-    print("Egg text exists locally!")
+    print("Egg text exists locally to terminal!")
 elif os.path.exists(file_path):     #check the Current Script file Directory for the text file
-    print("Egg text exists locally!")
+    print("Egg text exists locally to file!")
 else:
     print("Egg text has not been created yet!")
     print(os.getcwd())  #current working directory of terminal.
